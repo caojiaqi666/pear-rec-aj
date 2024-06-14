e22142a32f5c9a1b14f3546c302b252d988a9001
caojiaqi666 <1508771379@qq.com>
2024-06-14 20:10:12 +0800

    feat: 关于页

--- packages/web/src/pages/about/index.tsx
+++ packages/web/src/pages/about/index.tsx
@@ -1,6 +1,7 @@
 import ininitApp from '../../pages/main';
 import { Layout } from 'antd';
 import Close from '../../assets/close.png';
+import Logo from '../../assets/logo1.png';
 import styles from './index.module.scss';
 const { Header, Content } = Layout;
 const About = () => {
@@ -29,6 +30,13 @@ const About = () => {
   const closeWin = () => {
     window.electronAPI.sendAboutCloseWin();
   };
+  const config = {
+    logo: Logo,
+    softName: 'SnipTaker',
+    version: '4.7.2',
+    desc: 'SnipTaker Technology Co.,Ltd is a hi-tech corporation, specialized in providing professional solutions for Windows, Mac. Our goal is not only to provide high quality products but also provide comfortable customer service.',
+    bottomDesc: 'Coypright@2024 SnipTaker. All rights reserved.'
+  }
   return (
     <Layout>
       <Header className={styles.titlebar} style={headerStyle}>
@@ -37,7 +45,18 @@ const About = () => {
         </div>
 
       </Header>
-      <Content style={contentStyle}>About</Content>
+      <Content style={contentStyle}>
+        <div className={styles.header}>
+          <img className={styles.logo} src={config.logo} alt="" />
+          <div className={styles.softName}>{config.softName}</div>
+          <div className={styles.version}>Version：{config.version}</div>
+        </div>
+        <div className={styles.descWrap}>
+
+          <div className={styles.desc}>{config.desc}</div>
+          <div className={styles.bottomDesc}>{config.bottomDesc}</div>
+        </div>
+      </Content>
     </Layout>
   );
 };
